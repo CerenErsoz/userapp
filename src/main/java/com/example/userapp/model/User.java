bb50095eb4626bbc8f3bff72a47ea4e94bba5fab
Ceren Ersoz <ersozceren2@gmail.com>
2025-04-24 23:18:00 +0300

    Updated application.properties and User entity for H2 database.

--- src/main/java/com/example/userapp/model/User.java
+++ src/main/java/com/example/userapp/model/User.java
@@ -1,16 +1,19 @@
 package com.example.userapp.model;
 
+import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
 @Entity
+@Table(name = "users")
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private String name;
     private String email;
 
